820ae35713b520398cde740f43ac7e38ac27c89d
c-ue <cue08056@gmail.com>
2018-01-19 01:03:15 +0800

    avoid linux unknown connection
    add for 20180119 CDX contest

---
+++ autoclean.sh
@@ -0,0 +1,10 @@
+while [ true ]
+do
+	kill $(netstat -anop | grep -P '\d+\.\d+\.\d+\.\d+' \
+	| awk '{print $5 " " $7}'\
+	|grep -v "0.0.0.0:" \
+	|grep -v "10.211.55.2:" \
+	|awk '{print $2}' |grep -oP '\d+')
+	echo '[' $(date) ']:clean'
+	sleep 0.1
+done
