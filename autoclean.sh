0588dc4853b4c68263537dc42479c1504256b96f
c-ue <cue08056@gmail.com>
2018-01-19 19:03:10 +0800

    more compatibility
    use the extension rule expression of the grep

--- autoclean.sh
+++ autoclean.sh
@@ -1,15 +1,16 @@
 #################################
-#autoclean.sh
-#create for 20180119 CDX contest
-#please give me root privileges
+#script name: autoclean.sh
+#privilege: root
+#descript: kill all the connection without the target you agreement
 #################################
 while [ true ]
 do
-	kill $(netstat -anop | grep -P '\d+\.\d+\.\d+\.\d+' \
-	| awk '{print $5 " " $7}'\
-	|grep -v "0.0.0.0:" \
-	|grep -v "10.211.55.2:" \
-	|awk '{print $2}' |grep -oP '\d+')
-	echo '[' $(date) ']:clean'
-	sleep 0.1
+        kill -9  $(netstat -anop  | grep -E "([0-9]{1,3}[\.]){3}[0-9]{1,3}" \
+        | awk '{print $5 " " $7}'\
+        |grep -v "0.0.0.0:" \
+        |grep -v "127.0.0.1:" \
+        |grep -v "10.20.30.11:" \ 						#replace with the ip you agreement
+        |awk '{print $2}' |grep -oE "[0-9]{1,10}/" |grep -oE "[0-9]{1,10}")
+        echo '[' $(date) ']:clean'
+        sleep 0.1
 done
